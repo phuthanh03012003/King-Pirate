12dc999f68f46ffdaabc651e4d58cbc10b40ba21
Ethan_nguyen <phuthanh0301@gmail.com>
2024-12-22 20:27:33 -0600

    Add movement (key presses) and finish blue print for characters

--- laststand.py
+++ laststand.py
@@ -8,32 +8,90 @@ SREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH,SREEN_HEIGHT))
 pygame.display.set_caption('Last Stand')
 
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define player action variables
+moving_left = False
+moving_right = False
+
+#define BG color
+BG = (144, 201, 120)
+
+def draw_bg():
+    screen.fill(BG)    
+
 class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale):
+    def __init__(self, char_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load('img/player/Idle/0.png')
+        self.char_type = char_type
+        self.speed = speed 
+        self.direction = 1
+        self.flip = False
+        img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
         self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
+    def move(self, moving_left, moving_right):
+        #reset movement variables
+        dx = 0
+        dy = 0
+
+        #assign movement variables if moving left or right
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        #update position
+        self.rect.x += dx
+        self.rect.y += dy
+
     def draw(self):
-        screen.blit(self.image, self.rect)
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
-player = Soldier(200, 200, 3)  
-player2 = Soldier(400, 200, 3)  
-
+player = Soldier('player', 200, 200, 3, 5)
+enemy = Soldier('enemy', 400, 200, 3, 5)
 
 run = True
 while run:
+    clock.tick(FPS)
+
+    draw_bg()
 
     player.draw()
-    player2.draw()
+    enemy.draw()
+
+    player.move(moving_left, moving_right)
 
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
             run = False
+        
+        #keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
 
+        #Keyboard button released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+        
     pygame.display.update()
 
 pygame.quit()
